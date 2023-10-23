a769c8f94d2827c817e29ae3235e74825e4b7ac7
postemp <apostemp@mail.ru>
2023-10-23 22:45:00 +0300

    hw32 with getting items via SQL

--- server/src/main/java/ru/fisunov/http/server/ItemsWebApplication.java
+++ server/src/main/java/ru/fisunov/http/server/ItemsWebApplication.java
@@ -1,23 +1,47 @@
 package ru.fisunov.http.server;
-
+ 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemsWebApplication implements MyWebApplication {
     private String name;
     private List<Item> items;
 
+    private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String dbUser = "postgres";
+    private static final String dbPassword = "postgres";
+    private static final String SELECT_ITEMS = "select id, title from public.items";
+
+
     public ItemsWebApplication() {
         this.name = "Items Web Application";
-        this.items = List.of(
-                new Item(1L, "Box"),
-                new Item(2L, "Phone"),
-                new Item(3L, "Table"),
-                new Item(4L, "Monitor"),
-                new Item(5L, "Chair")
-        );
+//        this.items = List.of(
+//                new Item(1L, "Box"),
+//                new Item(2L, "Phone"),
+//                new Item(3L, "Table"),
+//                new Item(4L, "Monitor"),
+//                new Item(5L, "Chair")
+//        );
+
+        this.items = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, dbUser, dbPassword)) {
+            try (PreparedStatement ps = connection.prepareStatement(SELECT_ITEMS)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        long id = rs.getLong("id");
+                        String title = rs.getString("title");
+                        items.add(new Item(id,title));
+                    }
+                } catch (SQLException e) {
+                }
+            } catch (SQLException e) {
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Override
